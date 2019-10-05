d67c9ad3a429a7ef1c3dc0ed185e31a9961bea91
andres-a-h <andres.a.h@outlook.com>
2019-10-05 19:21:13 -0400

    fix

--- traffic.c
+++ traffic.c
@@ -15,7 +15,7 @@
 char *direction;
 
 /* function for thread in charge of handling a single car */
-void OneVehicle(char *direction) {
+void *OneVehicle(char *direction) {
   ArriveBridge(direction);
   // now the car is on the Bridge
 
@@ -26,24 +26,24 @@ void OneVehicle(char *direction) {
 }
 
 /* ArriveBridge(): helper function that does not return until safe to get on bridge */
-void ArriveBridge(char *direction) {
+void *ArriveBridge(char *direction) {
   fprintf(stdout, "I am getting on the bridge\n");
 }
 
 /* OnBridge(): helper function prints state of bridge and waiting cars */
-void OnBridge(char *direction) {
+void *OnBridge(char *direction) {
   fprintf(stdout, "I am the only car on the bridge\n");
 }
 
 /* ExitBridge(): helper function removes the car from the bridge */
-void ExitBridge(char *direction) {
+void *ExitBridge(char *direction) {
     fprintf(stdout, "I am exiting the bridge\n");
 }
 
 int main(int argc, char *argv[]) {
 
   pthread_t car1, car2;
-
+  *direction = TO_HANOVER;
   OneVehicle(TO_HANOVER);
 
   return 0;
