03cd8f112b045227ef5080d7be2616c388048bde
andres-a-h <andres.a.h@outlook.com>
2019-10-06 06:06:20 -0400

    fix

--- handlecar.c
+++ handlecar.c
@@ -28,6 +28,9 @@ void ArriveBridge(void *direction) {
     // obtain the lock if possible
     pthread_mutex_lock(&lock);
     unsigned int dir = (uintptr_t) direction;
+    if (dir == TO_HANOVER) {
+        fprintf(stdout, "\n\twe are going to hanover %d\n", dir);
+    }
     // wait until we are safely able to attempt to cross the bridge
     while ((!safeToHanover && (dir == TO_HANOVER)) || (!safeToNorwich && (dir == TO_NORWICH)) || active >= MAX_CARS) {
         pthread_cond_wait(&cond, &lock);
