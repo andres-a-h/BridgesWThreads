1020f1cc142afa9b55bdf3c59c0fd3a339bceab1
andres-a-h <andres.a.h@outlook.com>
2019-10-06 00:17:43 -0400

    fix

--- handlecar.c
+++ handlecar.c
@@ -10,7 +10,7 @@
 
 /* function for thread in charge of handling a single car */
 void *OneVehicle(void *direction) {
-    unsigned int x = (uintptr_t) direction; // source: https://stackoverflow.com/questions/26805461/why-do-i-get-cast-from-pointer-to-integer-of-different-size-error
+    unsigned int dir = (uintptr_t) direction; // source: https://stackoverflow.com/questions/26805461/why-do-i-get-cast-from-pointer-to-integer-of-different-size-error
 
     ArriveBridge(direction);
     // now the car is on the Bridge
--- traffic.c
+++ traffic.c
@@ -14,15 +14,23 @@
 #include "handlecar.h"
 
 #define MAX_CARS 3
-#define NUM_THREADS 3
 #define TO_HANOVER 0
 #define TO_NORWICH 1
 
 int main(int argc, char *argv[]) {
 
-    pthread_t car[NUM_THREADS];
+    pthread_t car[MAX_CARS];
     int rc;
-    for (int i = 0; i < NUM_THREADS; i++) {
+    pthread_mutex_t lock;
+
+    // create a mutex lock - return error if it fails
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        fprintf(stderr, "\n Error: Mutex creation failed!\n");
+        return 1;
+    }
+
+    // create a thread for each car
+    for (int i = 0; i < MAX_CARS; i++) {
         fprintf(stdout, "====> main() : creating thread\n");
         rc = pthread_create(&car[i], NULL, OneVehicle, (void *) TO_HANOVER);
 
